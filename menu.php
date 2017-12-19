2df15e5739b2452d611a6f731e24dd9aedaa5c02
Francisco Mat <francisco@f5sites.com>
2017-12-19 09:23:03 -0200

    vtiger menu

--- menu.php
+++ menu.php
@@ -12,8 +12,8 @@
 					<li><a href="/servicos.php">Servi&ccedil;os</a></li>
 					<a href="/index.php">Projetos</a></li>
 					<li><a href="/index.php">Advergames</a></li>
-					<li><a href="/promocao.php">Promo&ccedil;&atilde;o</a></li>
-					<li><a href="/vtiger.php">vTigerCRM</a></li>-->
+					<li><a href="/promocao.php">Promo&ccedil;&atilde;o</a></li-->
+					<li><a href="vtiger.php">vTigerCRM</a></li>
 					<li><a href="contato.php">Contato</a></li>
 				</ul>
 			</div>
